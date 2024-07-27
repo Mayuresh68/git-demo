2e162aea26dd6504027581f4126747020d34b014
Mayuresh Patil <mayuresh.pati@asmadiya.com>
2024-07-28 00:11:26 +0530

    new profile test component in feature

---
+++ src/app/new-profile-test/new-profile-test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-profile-test',
+  templateUrl: './new-profile-test.component.html',
+  styleUrls: ['./new-profile-test.component.css']
+})
+export class NewProfileTestComponent {
+
+}
