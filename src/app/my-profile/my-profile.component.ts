b3d8797c59fa0a5c5a8e55d28fe8fbdb2763dea9
Mayuresh Patil <mayuresh.pati@asmadiya.com>
2024-07-28 12:40:38 +0530

    Update my-profile component in feature/new-work branch

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent {
+  title = 'My Profile - Feature Branch';
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
